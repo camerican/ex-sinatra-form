1c5c30556611b6f25d39ed11df8cf37c4d9de21c
Cam Crews <camcrews@gmail.com>
2016-10-11 14:52:33 -0400

    Remaining character count example added

--- public/js/validate.js
+++ public/js/validate.js
@@ -1,4 +1,22 @@
+var elMessage, elMessageCharCount, elConsole;
+
 document.addEventListener("DOMContentLoaded",function(){
+  elMessage = document.querySelector("textarea[name=message]");
+  elMessageCharCount = document.getElementById("message-remaining");
+  elConsole = document.getElementById("console");
+  // Character Count update
+  updateRemaining(elMessage,elMessageCharCount);
+
+  elMessage.addEventListener("keydown",function(event){
+    // if there's still room to write, update the remaining character count and let them go through
+
+    // console.log( elMessage, elMessageCharCount );
+    if( !updateRemaining( elMessage, elMessageCharCount ) ) {
+      console.innerText = "Sorry, too many letters";
+    }
+    // else, if there's no room, block the letters
+  });
+
   document.querySelector("form").addEventListener("submit",function(event){
     console.log(event);
     // put our validation here...
@@ -14,3 +32,12 @@ document.addEventListener("DOMContentLoaded",function(){
     }
   });
 });
+// takes a number of characters and updates element
+function updateRemaining( elInput, elMsg ) {
+  var remaining = elInput.getAttribute('maxlength') - elInput.value.length;
+  if( remaining >= 0 ) {
+    elMsg.innerText = remaining;
+    return true;
+  } 
+  return false;
+}
